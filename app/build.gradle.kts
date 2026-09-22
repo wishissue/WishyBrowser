205cf4be3e1972d8ce977aa324eae5c225cceb50
Radrenders <catmadethismail@gmail.com>
2026-09-22 13:04:17 +0530

    Phase 1: Enable R8 full mode and prune resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,9 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            ndk {
+                debugSymbolLevel = "NONE"
+            }
         }
         debug {
             isMinifyEnabled = true
